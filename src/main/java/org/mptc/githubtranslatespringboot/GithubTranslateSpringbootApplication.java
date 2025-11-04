2c2c007e0882d578a0722c123843e134fe72273e
chento007 <chentochea2002@gmail.com>
2025-11-04 12:14:04 +0700

    first commit

---
+++ src/main/java/org/mptc/githubtranslatespringboot/GithubTranslateSpringbootApplication.java
@@ -0,0 +1,13 @@
+package org.mptc.githubtranslatespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubTranslateSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubTranslateSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/mptc/githubtranslatespringboot/GithubTranslateSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mptc.githubtranslatespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubTranslateSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
